62339b532b6ccd3d04c2a21097e53e201d5f7096
do-jade <leedohyun@ufl.edu>
2026-01-25 19:47:58 -0500

    Added sample input, initializedvariabled, and parsed input data for matcher

--- src/taskA.py
+++ src/taskA.py
@@ -1,2 +1,17 @@
+with open('tests/input.txt', 'r') as f:
+    lines = [line.strip() for line in f.readlines() if line.strip()]
 
+# n = number of hospitals/students
+n = int(lines[0])
+
+hospitalPreferences = []
+studentPreferences = []
+
+for i in range(1, n + 1):
+    preferences = list(map(int, lines[i].split()))
+    hospitalPreferences.append(preferences)
+
+for i in range(n + 1, 2 * n + 1):
+    preferences = list(map(int, lines[i].split()))
+    studentPreferences.append(preferences)
 
