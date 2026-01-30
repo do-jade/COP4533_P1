0eb3508398a1ad251327984dd307fff8a142e6fa
unknown <sriram.cjai@ufl.edu>
2026-01-30 02:05:46 -0500

    Finished Task B and tested both files (Only one concern)

--- src/taskB.py
+++ src/taskB.py
@@ -2,172 +2,184 @@
 # FILE IS JUST PSEUDOCODE FOR THE PURPOSE OF ENSURING LOGIC WORKS BEFORE IMPLEMENTING
 
 # Helper Functions
-# Read The File And Return A List Of Non-Empty Lines
-def readNonEmptyLines(filepath):
-    data = read_all_lines(filepath)
-    lineList = []
-    for line in data:
-        temp = trim(line)
-        if temp != "":
-            lineList.append(temp)
-    return lineList
-
 # Parses A Line Into Ints
-def intsFromLine(inp):
-    # Shed The Whitespace
-    pieces = split_whitespace(n)
-    intList = []
-    for piece in pieces:
-        # Will Probably Need An Edge Case
-        intList.append(to_int(piece))
-    return intList
+# https://www.w3schools.com/python/python_try_except.asp
+# https://www.w3schools.com/python/ref_string_strip.asp
+def lineToInts(givenLine):
+    pieces = givenLine.split()
+    if not pieces:
+        return []
+    try:
+        return [int(x) for x in pieces]
+    except ValueError:
+        raise ValueError("Non Int In Line: '" + givenLine.strip() + "'")
 
 # Checks If Provided Input Is A Valid Permutation
-def validPermutation(givenValues, n):
-    if len(givenValues) != n:
+# They Should Be Correct Length, In The Range Of 1 To n And No Dupes
+def validPermutationCheck(toCheck, n):
+    if len(toCheck) != n:
         return False
     checked = [False] * n
-    for x in givenValues:
-        if x < 1 or x > n:
+    for val in toCheck:
+        if val < 1 or val > n:
             return False
-        if checked[x - 1]:
+        temp = val - 1
+        if checked[temp]:
             return False
-        checked[x - 1] = True
+        checked[temp] = True
     return True
 
-# Might Need To Use More Than Once So I Made Them Functions
+# Originally Used Multiple Times, But Easier To Keep As Functions
 
 # Rank Tables During Comparison
-def createRankingTable(n, studentPreference):
-    # rankStudent[stud][hosp] = position of hospital in student's preference list
-    rankStudent = [[None] * n for _ in range(n)]
-    for stud in range(0, n):
-        for location in range(0, n):
-            hosp = studentPreference[stud][location]
-            rankStudent[stud][hosp] = location
-    return rankStudent
+# studentRank Ends Up As The Position Of The Hospital In The Students Preference List
+def createRankingTable(n, studentPref):
+    studentRanks = [[-1] * n for _ in range(n)]
+    for student in range(n):
+        for posit, hospital in enumerate(studentPref[student]):
+            studentRanks[student][hospital] = posit
+    return studentRanks
 
 # Checks For Blocking Pairs
-def blockingPairCheck(n, HospStud, StudHosp, HospPrefer, studRank):
-    # returns (stable, witness_string)
-    for hosp in range(0, n):
-        currentMatch = HospStud[hosp]
-        for stud in HospPrefer[hosp]:
-            if stud == currentMatch:
-                break
-            if studRank[stud][hosp] < studRank[stud][StudHosp[stud]]:
-                outpString = "(" + str(hosp + 1) + ", " + str(stud + 1) + ")"
-                return (False, outpString)
+# Returns True When Stable And (False, "hospital student") Otherwise
+def blockingPairCheck(n, hospitalToStudent, studentToHospital, hospitalPreference, studentRanks):
+    for hospital in range(n):
+        matchedStudent = hospitalToStudent[hospital]
 
-    return (True, "")
+        # Only Students With A Higher Rank Than matchedStudent Can Create A Blocking Pair
+        for student in hospitalPreference[hospital]:
+            if student == matchedStudent:
+                break
+
+            # If Student Ranks Higher Than Their Current Hospital
+            currentMatch = studentToHospital[student]
+            if studentRanks[student][hospital] < studentRanks[student][currentMatch]:
+                return False, str(hospital + 1) + " " + str(student + 1)
+    return True, ""
 
 # Main
 if __name__ == "__main__":
-    if argc != 3:
-        exit(1)
-    preferencePath = argv[1]
-    matchedPath = argv[2]
-
-    # Edge Cases
-    preferenceLines = readNonEmptyLines(preferencePath)
-    if len(preferenceLines) == 0:
-        print("Error: Preference File Is Empty")
-        exit(0)
+    # Will Go Invalid As Soon As Something Fails
     try:
-        n = to_int(preferenceLines[0])
-    except:
-        print("Error: First Line Isn't A Int")
-        exit(0)
+        # Read Preference File
+        with open("tests/input.txt", "r") as f:
+            preferences = [line.strip() for line in f if line.strip()]
 
-    if n <= 0:
-        print("Error: n Isn't Positive")
-        exit(0)
+        # Check For Empty File Edge Case
+        if not preferences:
+            raise ValueError("Preference File Is Empty")
 
-    expected = 1 + 2 * n
-    if len(preferenceLines) != expected:
-        print("Error: Supposed To Have " + str(expected) + " Lines, But Got " + str(len(preferenceLines)) + " Lines Instead")
-        exit(0)
-
-    # Parse Hospital Prefs
-    hospPref = [None] * n
-    for h in range(0, n):
-        line = preferenceLines[1 + h]
+        # Take First Line And Designate N
         try:
-            vals = intsFromLine(line)
-        except:
-            exit(0)
+            n = int(preferences[0])
+        except ValueError:
+            raise ValueError("n Needs To Be The First Line")
 
-        if not validPermutation(vals, n):
-            exit(0)
+        # Check For n Being Valid
+        if n <= 0:
+            raise ValueError("n Has To Be Positive")
 
-        hospPref[h] = [x - 1 for x in vals]
+        #print("DEBUG n:", n)
+        #print("DEBUG len(preferences):", len(preferences))
+        #print("DEBUG [repr(x) for x in preferences]:", [repr(x) for x in preferences])
 
-    # ---- Parse student prefs ----
-    studPref = [None] * n
-    for s in range(0, n):
-        line = preferenceLines[1 + n + s]
+        # Verify Input File Has Correct Number Of Lines
+        totalInputLines = 1 + 2 * n
+        if len(preferences) != totalInputLines:
+            raise ValueError("Incorrect Number Of Input Lines, Should Be: " + str(totalInputLines) + ". Instead Got: " + str(len(preferences)))
+
+        # Go Through Hospital Preferences
+        hospitalPreference = []
+        for hospitalIndex in range(n):
+            temp = lineToInts(preferences[1 + hospitalIndex])
+
+            if not validPermutationCheck(temp, n):
+                raise ValueError("Hosptial Preferences Are Not Valid Permutations, Index: " + str(hospitalIndex + 1))
+
+            # 0 Indexing
+            hospitalPreference.append([x - 1 for x in temp])
+
+        # Go Through Student Preferences
+        studentPreference = []
+        for studentIndex in range(n):
+            temp = lineToInts(preferences[1 + n + studentIndex])
+
+            if not validPermutationCheck(temp, n):
+                raise ValueError("student prefs not a permutation at student " + str(studentIndex + 1))
+
+            # 0 Indexing
+            studentPreference.append([x - 1 for x in temp])
+
+        # Read Matching Verification File
+        #with open("tests/matching.txt", "r") as f:
+        #    matches = [line.strip() for line in f if line.strip()]
+        # https://stackoverflow.com/questions/4655250/difference-between-utf-8-and-utf-16
+        # FOR SOME REASON THIS HAS TO ACCOUNT FOR BOTH FILE VERSIONS DO NOT REMOVE THE UTF 16 PART PLEASE
         try:
-            vals = intsFromLine(line)
-        except:
-            exit(0)
+            with open("tests/matching.txt", "r", encoding="utf-8") as f:
+                matches = [line.strip() for line in f if line.strip()]
+        except UnicodeDecodeError:
+            with open("tests/matching.txt", "r", encoding="utf-16") as f:
+                matches = [line.strip() for line in f if line.strip()]
 
-        if not validPermutation(vals, n):
-            exit(0)
+        #print("DEBUG n =", n)
+        #print("DEBUG len(matches) =", len(matches))
+        #print("DEBUG [repr(x) for x in matches]", [repr(x) for x in matches])
+        #with open("tests/matching.txt", "rb") as bf:
+        #    print("DEBUG bf.read(30) =", bf.read(30))
 
-        studPref[s] = [x - 1 for x in vals]
+        # Number Of Matches Should Match n
+        if len(matches) != n:
+            raise ValueError("Incorrect Number Of Matching Input Lines, Should Be: " + str(n) + ". Instead Got: " + str(len(matches)))
 
-    # ---- Read matching file ----
-    matchingLine = readNonEmptyLines(matchedPath)
-    if len(matchingLine) != n:
-        exit(0)
+        hospitalToStudent = [-1] * n
+        studentToHospital = [-1] * n
+        for line in matches:
+            matchingPair = lineToInts(line)
 
-    hospToStud = [-1] * n
-    studToHosp = [-1] * n
+            # Check Matching Input
+            if len(matchingPair) != 2:
+                raise ValueError("Match Line Should Only Have 2 Ints: '" + line + "'")
+            pairHospital, pairStudent = matchingPair
+            hospitalIndex = pairHospital - 1
+            studentIndex = pairStudent - 1
 
-    for line in matchingLine:
-        try:
-            pair = intsFromLine(line)
-        except:
-            exit(0)
+            # Validate Individual Pair
+            if not (0 <= hospitalIndex < n) or not (0 <= studentIndex < n):
+                raise ValueError("Pair Is Not Valid: " + str(pairHospital) + " " + str(pairStudent))
 
-        if len(pair) != 2:
-            exit(0)
+            # Check For Uniqueness
+            if hospitalToStudent[hospitalIndex] != -1:
+                raise ValueError("Hospital " + str(pairHospital) + " Is Matched Multiple Times")
+            if studentToHospital[studentIndex] != -1:
+                raise ValueError("Student " + str(pairStudent) + " Is Matched Multiple Times")
+            hospitalToStudent[hospitalIndex] = studentIndex
+            studentToHospital[studentIndex] = hospitalIndex
 
-        i = pair[0]   
-        j = pair[1]   
-        h = i - 1
-        s = j - 1
+        # Ensure Everyone Is Matched
+        for hospitalIndex in range(n):
+            if hospitalToStudent[hospitalIndex] == -1:
+                raise ValueError("Hospital " + str(hospitalIndex + 1) + " Is Not Matched")
+        for studentIndex in range(n):
+            if studentToHospital[studentIndex] == -1:
+                raise ValueError("Student " + str(studentIndex + 1) + " Is Not Matched")
 
-        if h < 0 or h >= n or s < 0 or s >= n:
-            exit(0)
+    # If Unable To Locate File
+    except FileNotFoundError as err:
+        print("INVALID (File Not Found: " + str(err) + ")")
+        raise SystemExit(0)
+    
+    # Value Errors
+    except ValueError as err:
+        print("INVALID (" + str(err) + ")")
+        raise SystemExit(0)
 
-        if hospToStud[h] != -1:
-            exit(0)
+    # Find Blocking Pairs
+    studentRanks = createRankingTable(n, studentPreference)
+    result = blockingPairCheck(n, hospitalToStudent, studentToHospital, hospitalPreference, studentRanks)
 
-        if studToHosp[s] != -1:
-            exit(0)
-
-        hospToStud[h] = s
-        studToHosp[s] = h
-
-    # ---- Inline validity check (bijection + inverse) ----
-    # Check all hospitals matched
-    for h in range(0, n):
-        if hospToStud[h] == -1:
-            exit(0)
-
-    # Check all students matched + inverse consistency
-    for s in range(0, n):
-        if studToHosp[s] == -1:
-            exit(0)
-
-    for h in range(0, n):
-        s = hospToStud[h]
-        if studToHosp[s] != h:
-            exit(0)
-
-    # Rank Table
-    studRank = createRankingTable(n, studPref)
-
-    # Check Stability
-    stable, witness = blockingPairCheck(n, hospToStud, studToHosp, hospPref, studRank)
+    # Print Results
+    if result[0]:
+        print("VALID STABLE")
+    else:
+        print("UNSTABLE (Blocking Pair: " + result[1] + ")")
